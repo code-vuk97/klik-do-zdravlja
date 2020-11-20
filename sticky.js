652787214271f4be14bb6e92d76f0a41b848025b
code-vuk97 <stefanovicvuk97@gmail.com>
2020-11-20 22:17:07 +0100

    Fix

--- sticky.js
+++ sticky.js
@@ -1,7 +1,6 @@
 // When the user scrolls the page, execute myFunction
-window.onscroll = function () {
-  myFunction();
-};
+var rightContainer = document.querySelector('.split.right') || window;
+rightContainer.addEventListener('scroll', myFunction);
 
 // Get the navbar
 var navbar = document.getElementById("nav");
@@ -11,9 +10,10 @@ var sticky = navbar.offsetTop;
 
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function myFunction() {
-  if (window.pageYOffset >= 49) {
+  if (window.pageYOffset >= 49 || rightContainer.scrollTop >= 49) {
     navbar.style.position = "sticky";
     navbar.style.top = "0";
+    rightContainer.style.paddingTop = 0;
   } else {
     navbar.style.position = "absolute";
     navbar.style.top = "50px";
